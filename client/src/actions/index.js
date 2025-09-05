98a494eb39992f38db6ea5cfa12998d0f3e2f369
Monir Hossain <monirhdigital@gmail.com>
2025-09-05 21:01:25 +0600

    final check

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,7 +6,7 @@ export async function explanation (prevState, formData){
         const language = formData.get("language");
         const code = formData.get("code");
         // const base_url = import.meta.env.VITE_API_URL;
-        const result = await fetch(`http://localhost:8000/api/code-explain`,{
+        const result = await fetch(`https://code-explain-ai.vercel.app/api/code-explain`,{
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({ language, code }),
